08fd292070a1346fc00447c299917727f875f5af
Muhammad Rizwan <34979598+MRizwanSE@users.noreply.github.com>
2019-02-19 00:18:56 +0500

    Add files via upload

---
+++ CircularQ.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using static System.Console;
+
+namespace CircularQueue
+{
+    public class CircularQ
+    {
+        private int[] QU;
+        private int F, B;
+        public CircularQ(int size)
+        {
+            QU = new int[size];
+            F = B = -1;
+        }
+        public void Enqueue(int x)
+        {
+            if (F == (B + 1) % QU.Length)
+            {
+                WriteLine("CQ is Full");
+                return;
+            }
+            else
+            {
+                B = (B + 1) % QU.Length;
+                QU[B] = x;
+                WriteLine($"Value {x} is Inserted!");
+            }
+            if (F == -1)
+                F = 0;
+        }
+        public void Dequeue()
+        {
+            if (F == -1 && B == -1)
+            {
+                WriteLine("CQ is Empty");
+                return;
+            }
+            else
+            {
+                WriteLine($"Value {QU[F]} is removed!");
+                QU[F] = 0;
+            }
+            if (F == B)
+                F = B = -1;
+            else
+                F = (F + 1) % QU.Length;
+        }
+    }
+}
